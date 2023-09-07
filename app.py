8100d3de68a692abaf366215d5d48848c0a5f22b
Maria Clara <mariaclarasaibert@hotmail.com>
2023-09-07 01:40:52 +0000

    rota com mock

--- app.py
+++ app.py
@@ -1,19 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, abort, flash, redirect, url_for
+from posts import posts
 
 app = Flask("Meu app")
+app.config['SECRET_KEY'] = 'pudim'
 
-posts = [
-    {
-        "titulo": "Minha primeira postagem!",
-        "texto": "teste texto"
-    },
-    {
-        "titulo": "Segundo post",
-        "texto": "teste texto 2"
-    }
-]
 
 @app.route('/')                      #para definir a rota do codigo, posso dar qlqr nome entre aspas simples
 def exibir_entradas():               #conjunto de ações
-    entradas = posts                 #mock das postagens
+    entradas = posts[::-1]                #mock das postagens
     return render_template('exibir_entradas.html', entradas=entradas)           #conjunto de ações
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST"
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logado'] = True
+            flash("Login efetuado com sucesso!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"        
+    return render_template('login.html', erro=erro)
+
+@app.route('/logout')
+def logout():
+    session.pop('logado')
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/inserir', methods=["POST"])
+def inserir_entradas():
+    if session['logado']:
+        novo_post = {
+            "titulo": request.form['titulo'],
+            "texto": request.form['texto']
+        }
+        posts.append(novo_post)
+        flash("Post criado com sucesso!")
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/posts/<int:id>')
+def exibir_entrada(id):
+    try:
+        entrada = posts[id-1]
+        return render_template('exibir_entrada.html', entrada=entrada)
+    except Exception:
+        return abort(404)
